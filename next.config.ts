4807fd6650f2c5e34874c5d3bd6ff2d8aa34767e
golfsilly <pnpnd2107@gmail.com>
2026-06-12 15:25:50 +0700

    edit config ก่อน build v1.0.0

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
+  output: "standalone",
+  compress: true,
+  poweredByHeader: false,
 
   devIndicators: {},
 
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
         protocol: "https",
         hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.4.30",
       },
       {
         protocol: "https",
@@ -27,24 +38,10 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "**.res.cloudinary.com",
-      },
     ],
   },
 
-  env: {
-    NEXT_PUBLIC_APP_VERSION: "0.1.0",
-  },
-
-  allowedDevOrigins: [
-    "localhost",
-    "127.0.0.1",
-    "192.168.1.116",
-    "172.16.40.147",
-    "10.212.134.10",
-  ],
+  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.4.30"],
 };
 
 export default nextConfig;
