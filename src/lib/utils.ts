f76c0f624b8372359502ac39abbe9a8ddb821a38
golfsilly <pnpnd2107@gmail.com>
2026-06-12 16:09:02 +0700

    ready build v1.0.0

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,19 +13,7 @@ export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_URL}${path}`;
 }
 
-export async function getSetting(key: string) {
-  return prisma.systemSetting.findUnique({
-    where: { key },
-  });
-}
 
-export async function getJsonSetting<T>(key: string): Promise<T | null> {
-  const setting = await prisma.systemSetting.findUnique({
-    where: { key },
-  });
-
-  return setting?.valueJson as T;
-}
 
 export const thaiDate = new Date().toLocaleDateString("th-TH", {
   day: "numeric",
